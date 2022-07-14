b221cf51673ff58e98680d0846a27e3e25c215c1
Zi Yi Koh <kohziyi95@gmail.com>
2022-07-14 19:57:19 +0800

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
 
 export default function App() {
+
+  const [loading, setLoading] = useState(true);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus Arrival Time:</Text>
+      <Text style={styles.arrivalTime}>{loading? "Loading" : "Loaded"}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => setLoading(true)}>
+        <Text style={styles.buttonText}>Refresh!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +19,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  title: {
+    fontSize: 50,
+    marginVertical: 20,
+  },
+  button: {
+    backgroundColor: "lightblue",
+    padding: 20,
+    marginVertical: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+  }
 });
